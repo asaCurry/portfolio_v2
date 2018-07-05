e35d57be82e95eb8341127a014ad95ce7198ee36
Asa Curry <asacurry80@gmail.com>
2018-07-05 17:52:33 -0400

    additional dev setup and debug

--- server.js
+++ server.js
@@ -8,43 +8,22 @@ const axios = require('axios');
 app.set('views', './views');
 app.set('view engine', 'pug');
 
+app.use(express.static(__dirname + '/public'));
 
-// GET response for '/'
+
+// Views
 app.get('/', function (req, res) {
-    // render the 'index' template, and pass in a few variables
-    const url = 'https://api.hubapi.com/hubdb/api/v2/tables/683481/rows/draft';
-    axios.get(url, {
-    params: {
-      hapikey: '5660e8c7-016f-4ffb-a32f-55d0aee4b890'
-    }
-  })
-    .then(response => {
-      console.log("the request status is: " + response.status);
 
-      let data = response.data.objects;
-      let cityList =[];
+res.render('layout', {
 
-      data.map((city) => {
-        cityList.push(city.name)
-      })
-      res.render('index', {
-        title: 'Hey',
-        message: 'Hello there!',
-        cities: cityList
-      })
-
-    })
-    .catch(error => {
-      console.log(error);
-    });
+  });
 });
 
 
 app.get('/home', function (req, res) {
-    // render the 'index' template, and pass in a few variables
-    res.render('index', {
-      title: 'Home',
-      message: 'Hello there!',
+
+    res.render('layout', {
+
     })
 });
 // start up the server
